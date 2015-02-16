22e851208741cd24f3de72df23153effcb2d4d30
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-16 12:06:25 +0000

    minor -- separation;

--- crisp_syntax.ml
+++ crisp_syntax.ml
@@ -231,12 +231,14 @@ let rec expression_to_string indent = function
     expression_to_string 0 b2 ^ "))"
   | Not b' ->
     indn indent ^ "(not " ^ expression_to_string 0 b' ^ ")"
+
   | ITE (be, e1, e2) ->
     indn indent ^ "if " ^
     expression_to_string 0 be ^ ":\n" ^
     expression_to_string (indent + indentation) e1 ^ "\n" ^
     indn indent ^ "else:\n" ^
     expression_to_string (indent + indentation) e2
+
   | Update (value_name, expression) ->
     (*NOTE for proper pretty-printing we can use width-senstitive generation of
            code blocks, as is standard. Currently this approach is crude, to get

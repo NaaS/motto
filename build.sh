f09fcd90b1d9e0ef224adc81b4e465781f69baf9
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-23 07:44:14 +0000

    including IL in building;

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ ocamlbuild -cflag -g -lflag -g -tag thread -use-ocamlfind -use-menhir \
   -package core \
   -no-hygiene \
   crisp_test.byte
+
+ocamlbuild -cflag -g -lflag -g -tag thread -use-ocamlfind -use-menhir \
+  -package core \
+  -no-hygiene \
+  naasty.byte

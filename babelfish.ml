8cff4d68496c19b47a331cfb2d3a27f4b0c644d3
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-25 11:43:28 +0000

    snapshot commit while evolving other bits of the system;

--- babelfish.ml
+++ babelfish.ml
@@ -13,16 +13,22 @@ type state =
     type_symbols : (string * integer) list
   }
 
-
+(*
 (*FIXME i'm ignoring annotations for the time being*)
 let rec naasty_of_flick_type (st : state) (declaration : bool)
   : (type_value -> naasty_type option * state) = function
   | Empty -> failwith "Cannot translate empty type"
+  | Tuple (label_opt, []) ->
+    (*FIXME ignoring label_opt*)
+    ((Some Unit_Type, st))
   | UserDefinedType (label_opt, type_name) ->
     assert (not declaration);
     assert (List.mem_assoc type_name st.type_symbols);
     let type_name' = List.assoc type_name st.type_symbols in
     (*FIXME ignoring label*)
+(*
+      opt_string (indn indent) label " : " ^ "type " ^ type_name ^ endline
+*)
     let ty' =
       Some (UserDefined_Type (type_name', None(*FIXME*)))
     in (ty', st)
@@ -42,3 +48,13 @@ let rec naasty_of_flick_type (st : state) (declaration : bool)
   | Reference (label_opt, ty) -> None, st
 *)
 
+
+(*FIXME crude test*)
+Record_Type (0, [(1, Int_Type {signed = true; precision = 32});
+                 (2, Bool_Type);
+                 (3, String_Type);
+                 (4, Array_Type (Int_Type {signed = false; precision = 64},
+                                 Some 4))])
+|> string_of_naasty_type prog_indentation true
+|> print_endline
+*)

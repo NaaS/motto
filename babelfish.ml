ac4dd8ceff3a03784b598b265cf74a5a68692203
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-25 14:32:06 +0000

    added translated of UDT; made error handling emit more helpful messages; added non-handling of unions;

--- babelfish.ml
+++ babelfish.ml
@@ -10,34 +10,48 @@ open Naasty
 type state =
   { pragmas : string list;
     type_declarations : naasty_type list;
-    type_symbols : (string * integer) list
+    next_typesymbol : integer;
+    type_symbols : (string * integer) list;
+    next_symbol : integer;
+    symbols : (string * integer) list;
   }
 
-(*
 (*FIXME i'm ignoring annotations for the time being*)
-let rec naasty_of_flick_type (st : state) (declaration : bool)
-  : (type_value -> naasty_type option * state) = function
+let rec naasty_of_flick_type (st : state)
+  : (type_value -> naasty_type option(*FIXME why option?*) * state) = function
+  | Disjoint_Union (_, _) -> failwith "Unsupported"
   | Empty -> failwith "Cannot translate empty type"
   | Tuple (label_opt, []) ->
-    (*FIXME ignoring label_opt*)
+    assert (label_opt = None);
+    (*We cannot have values of type "void" in the target, we can only type
+      functions with such a type.*)
     ((Some Unit_Type, st))
   | UserDefinedType (label_opt, type_name) ->
-    assert (not declaration);
-    assert (List.mem_assoc type_name st.type_symbols);
+    if not (List.mem_assoc type_name st.type_symbols) then
+      failwith ("Undeclared type: " ^ type_name);
     let type_name' = List.assoc type_name st.type_symbols in
-    (*FIXME ignoring label*)
-(*
-      opt_string (indn indent) label " : " ^ "type " ^ type_name ^ endline
-*)
+    let (label_opt', st') =
+      begin
+        match label_opt with
+        | None -> (None, st)
+        | Some s ->
+          if (List.mem_assoc s st.symbols) then
+            (*shadowing is forbidden*)
+            failwith ("Already declared: " ^ s);
+          (Some st.next_symbol,
+           { st with
+             symbols = (s, st.next_symbol) :: st.symbols;
+             next_symbol = 1 + st.next_symbol;
+           })
+      end in
     let ty' =
-      Some (UserDefined_Type (type_name', None(*FIXME*)))
-    in (ty', st)
+      Some (UserDefined_Type (label_opt', type_name'))
+    in (ty', st')
 (*
   | String (label_opt, type_ann)
   | Integer (label_opt, type_ann)
   | Boolean (label_opt, type_ann)
   | RecordType (label_opt, tys, type_ann)
-  | Disjoint_Union (label_opt, tys)
   | List (label_opt, ty, _, type_ann)
   | IPv4Address label_opt
   (*We send records, not tuples, over the wire, so tuples
@@ -49,12 +63,12 @@ let rec naasty_of_flick_type (st : state) (declaration : bool)
 *)
 
 
-(*FIXME crude test*)
+(*FIXME crude test
 Record_Type (0, [(1, Int_Type {signed = true; precision = 32});
                  (2, Bool_Type);
                  (3, String_Type);
                  (4, Array_Type (Int_Type {signed = false; precision = 64},
                                  Some 4))])
 |> string_of_naasty_type prog_indentation true
-|> print_endline
+  |> print_endline
 *)

ad709c2536788e3a87904e5aeb2e4710f4b03c4e
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-27 20:06:04 +0000

    refined data-model's metadata, as encoded here;

--- serialisation.ml
+++ serialisation.ml
@@ -21,6 +21,13 @@ type translated_type =
     serialiser : naasty_function option;
     deserialiser : naasty_function option }
 
+type data_model_component =
+  {
+    name : string;
+    identifiers : string list;
+    scheme : naasty_type;
+  }
+
 (*
   Based on
    https://lsds.doc.ic.ac.uk/gitlab/naas/naas-box-system/blob/master/src/applications/hadoop_data_model/HadoopDMData.h 
@@ -35,53 +42,70 @@ type translated_type =
   size_t get_stream_len();
 *)
 let get_channel_len =
-  (["get_channel_len"],
-   Fun_Type (-1, Size_Type None, []))
+  { name = "get_channel_len";
+    identifiers = ["get_channel_len"];
+    scheme = Fun_Type (-1, Size_Type None, [])}
 let get_stream_len =
-  (["get_stream_len"],
-    Fun_Type (-1, Size_Type None, []))
+  { name = "get_stream_len";
+    identifiers = ["get_stream_len"];
+    scheme = Fun_Type (-1, Size_Type None, [])}
 let bytes_stream_to_channel datatype =
-  (["bytes_stream_to_channel";
-    datatype;
-    "stream";
-    "channel";
-    "streamend";
-    "bytes_read";
-    "bytes_written"],
-   Fun_Type (-1,
-             Static_Type (None,
-                          Reference_Type (None,
-                                          UserDefined_Type (None, -2))),
-             [Reference_Type (Some (-3), Char_Type None);
-              Reference_Type (Some (-4), Char_Type None);
-              Reference_Type (Some (-5), Char_Type None);
-              Reference_Type (Some (-6), Size_Type None);
-              Reference_Type (Some (-7), Size_Type None)]))
+  { name = "bytes_stream_to_channel";
+    identifiers =
+      ["bytes_stream_to_channel";
+       datatype;
+       "stream";
+       "channel";
+       "streamend";
+       "bytes_read";
+       "bytes_written"];
+    scheme =
+      Fun_Type (-1,
+                Static_Type (None,
+                             Reference_Type (None,
+                                             UserDefined_Type (None, -2))),
+                [Reference_Type (Some (-3), Char_Type None);
+                 Reference_Type (Some (-4), Char_Type None);
+                 Reference_Type (Some (-5), Char_Type None);
+                 Reference_Type (Some (-6), Size_Type None);
+                 Reference_Type (Some (-7), Size_Type None)])}
 let write_bytes_to_channel datatype =
-  (["write_bytes_to_channel";
-    datatype;
-    "channel";
-    "no_bytes"],
-   Fun_Type (-1,
-             Static_Type (None,
-                          Unit_Type),
-             [Reference_Type (None, UserDefined_Type (None, -2));
-              Reference_Type (Some (-3), Char_Type None);
-              Reference_Type (Some (-4), Size_Type None)]))
+  { name = "write_bytes_to_channel";
+    identifiers =
+      ["write_bytes_to_channel";
+       datatype;
+       "channel";
+       "no_bytes"];
+    scheme =
+      Fun_Type (-1,
+                Static_Type (None,
+                             Unit_Type),
+                [Reference_Type (None, UserDefined_Type (None, -2));
+                 Reference_Type (Some (-3), Char_Type None);
+                 Reference_Type (Some (-4), Size_Type None)])}
 let bytes_channel_to_stream =
-  (["bytes_channel_to_stream";
-    "stream";
-    "channel";
-    "bytes_read";
-    "bytes_written"],
-   Fun_Type (-1,
-             Static_Type (None,
-                          Unit_Type),
-             [Reference_Type (Some (-2), Char_Type None);
-              Reference_Type (Some (-3), Char_Type None);
-              Reference_Type (Some (-4), Size_Type None);
-              Reference_Type (Some (-5), Size_Type None)]))
+  { name = "bytes_channel_to_stream";
+    identifiers =
+      ["bytes_channel_to_stream";
+       "stream";
+       "channel";
+       "bytes_read";
+       "bytes_written"];
+    scheme =
+      Fun_Type (-1,
+                Static_Type (None,
+                             Unit_Type),
+                [Reference_Type (Some (-2), Char_Type None);
+                 Reference_Type (Some (-3), Char_Type None);
+                 Reference_Type (Some (-4), Size_Type None);
+                 Reference_Type (Some (-5), Size_Type None)])}
 
+(*Instantiates the data model for a particular serialisable datatype.
+  This should generate part of the "struct" definition in the resulting C++
+  translation of the Flick type. *)
+let instantiate_data_model datatype =
+  [get_channel_len; get_stream_len; bytes_stream_to_channel datatype;
+   write_bytes_to_channel datatype; bytes_channel_to_stream]
 
 (*
 How this works:
@@ -103,7 +127,7 @@ let gen_deserialiser (ty : type_value) : naasty_function =
 ;;
 (*FIXME crude test*)
 fold_map ([], initial_state) (fun st scheme ->
-      instantiate true (fst scheme) st (snd scheme))
+      instantiate true scheme.identifiers st scheme.scheme)
   [get_channel_len; get_stream_len; bytes_stream_to_channel "test";
    write_bytes_to_channel "test"; bytes_channel_to_stream]
 |> (fun (tys, st) ->

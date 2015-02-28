3bb1984bf1d1db6454b336af5fda0d2c198ab680
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-02-28 12:16:54 +0000

    added project model for Flick;

---
+++ crisp_project.ml
@@ -0,0 +1,15 @@
+(*
+   Model of the organisation of Flick code.
+   Nik Sultana, Cambridge University Computer Lab, February 2015
+*)
+
+type compilation_unit =
+  { name : string;
+    (*NOTE overlap of "inclusions" with state.inclusions*)
+    inclusions : string list;
+    content : Crisp_syntax.program }
+
+let filename_of_compilationunit (cu : compilation_unit) : string =
+  cu.name ^ ".cp"
+
+type project = compilation_unit list

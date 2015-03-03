3d64142007c33bb4f53aa1ac1626a3594f1028cd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-03-03 12:26:38 +0000

    added note;

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ ocamlbuild -cflag -g -lflag -g -tag thread -use-ocamlfind -use-menhir \
   -no-hygiene \
   crisp_test.byte
 
+# NOTE could add -dont-catch-errors to have exceptions pass through catches.
 ocamlbuild -cflag -g -lflag -g -tag thread -use-ocamlfind -use-menhir \
   -package core \
   -no-hygiene \

ec7b19bd8ca8bdd4f99a43a815b1e6e6b9edda0c
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-03-03 12:27:48 +0000

    added simple support function, to be used when translating function signatures;

--- crisp_syntax_aux.ml
+++ crisp_syntax_aux.ml
@@ -8,3 +8,7 @@ open Crisp_syntax
 let name_of_type = function
   | Type ty_decl -> ty_decl.type_name
   | _ -> failwith "Expected type declaration."
+
+(*Unwraps a Crisp function type into a tuple of its components*)
+let extract_function_types (FunType (FunDomType (chans, arg_tys), FunRetType ret_tys)) =
+  ((chans, arg_tys), ret_tys)

5669ced6e749009da42fc77a0d52b11319ca0a0a
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-03-11 20:56:39 +0000

    more elaboration of the model;

--- task_model.ml
+++ task_model.ml
@@ -8,26 +8,36 @@ type task_type =
   | Processing
   | Output
 
+(*FIXME need inference to determine Task subclass? e.g., Many2One*)
+(*FIXME since different subclasses have different constructors, and perhaps
+        additional methods/members, each kind of class represented here must be
+        associated with whatever additional methods/members/constructor
+        parameters that class expects.*)
+type task_class =
+  | Many2One
+  | Task
+  (*FIXME incomplete*)
+
 type chan_type =
   | Socket (*FIXME encode socket metadata -- address and port?*)
   | Type (*FIXME encode type data*)
 
-type channel =
+(*NOTE channels in libNaaS are unidirectional*)
+type chan =
   {
-    chan_id : int; (*FIXME needed?*)
+    (*NOTE a channel's id consists of its offset in input_chans or
+           output_chans*)
     chan_type : chan_type;
   }
 
-(*FIXME need inference to determine Task subclass? e.g., Many2One*)
-
 type task =
   {
-    task_id : int;
+    task_id : int; (*NOTE must be unique*)
     task_type : task_type;
-    (*NOTE channels in libNaaS are unidirectional*)
-    input_channels : unit(*FIXME *);
-    output_channels : unit(*FIXME as above*);
+    task_class : task_class;
+    input_chans : chan list;
+    output_chans : chan list;
     process : Crisp_syntax.process;
   }
 
-type task_graph = task list
+type task_graph = task list(*FIXME incomplete*)

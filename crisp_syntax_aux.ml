c17ce36027c65f46e025cb53f3aa488295666680
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-06-19 16:24:51 +0100

    sketch of type inference;

--- crisp_syntax_aux.ml
+++ crisp_syntax_aux.ml
@@ -70,3 +70,109 @@ let update_empty_label label (ty : type_value) =
 let the_ty_of_decl = function
   | Type ty_decl -> ty_decl.type_value
   | _ -> failwith "Was expecting a type declaration."
+
+(*NOTE we only check the outer constants -- we don't traverse inside the
+  expression*)
+let rec ty_of_expr ?strict:(strict : bool = false) (env : (string * type_value) list) : expression -> type_value = function
+  | Variable label ->
+    begin
+      try List.assoc label env
+      with Not_found ->
+        failwith ("Missing declaration for '" ^ label ^ "'")
+    end
+
+  (*Boolean expressions*)
+  | True
+  | False
+    -> Boolean (None, [])
+  | And (e1, e2)
+  | Or (e1, e2) ->
+    let ans = Boolean (None, []) in
+    if strict then
+      let f = ty_of_expr ~strict env in
+      let (e1_ty, e2_ty) = f e1, f e2 in
+      assert (e1_ty = e2_ty);
+      assert (e1_ty = ans)
+    else ();
+    ans
+  | Not _
+
+  (*Definable over arbitrary types of expressions*)
+  | Equals (_, _)
+
+  (*Definable over arithmetic expressions*)
+  | GreaterThan (_, _)
+  | LessThan (_, _)
+    -> Boolean (None, [])
+
+  (*Arithmetic expressions*)
+  | Int _ -> Integer (None, [])
+
+  (*NOTE for these expressions we might want to look deeper, to differentiate
+    between different kinds of numbers -- ints, floats, etc*)
+  | Plus (_, _)
+  | Minus (_, _)
+  | Times (_, _)
+  | Mod (_, _)
+  | Quotient (_, _)
+  | Abs _ ->
+    Integer (None, [])
+
+  (*Native representation of an IPv4 address*)
+  | IPv4_address (_, _, _, _) -> IPv4Address None
+  (*Integer to IP address*)
+  | Int_to_address _ -> IPv4Address None
+  (*IP address to integer*)
+  | Address_to_int _ -> Integer (None, [])
+
+  (*NOTE currently we don't support dependently-typed lists*)
+  | _ -> failwith ("TODO")
+(*
+  | EmptyList
+  | ConsList of expression * expression
+  | AppendList of expression * expression
+
+  | TupleValue of expression list
+
+  | Seq of expression * expression
+  | ITE of expression * expression * expression option
+  | LocalDef of typing * expression (*def value_name : type = expression*)
+  | Update of value_name * expression (*value_name := expression*)
+  (*value_name[idx] := expression*)
+  | UpdateIndexable of value_name * expression * expression
+
+  (*This work for both tuples and records.*)
+  | Projection of expression * label
+
+  | Function_Call of function_name * fun_arg list
+
+  | Record of (label * expression) list
+  | RecordUpdate of (expression * (label * expression))
+
+  (*Case elimination on variants; formation of variant
+    instances will look like function application in the
+    language, therefore it doesn't require special syntax.*)
+  | CaseOf of expression * (expression * expression) list
+
+  (*The first parameter could be generalised to an expression,
+    but I don't think we need that expressiveness at the moment.
+    Also, the second parameter could be specialised to a natural
+    number -- we might go for that for the moment.*)
+  | IndexableProjection of label * expression
+
+  | IntegerRange of expression * expression
+  | Map of label * expression * expression * bool
+  | Iterate of label * expression *
+               (label * expression) option *
+               expression * bool
+
+  (*Channel operations. Can be overloaded to, say, send values
+    on a channel, or to first obtain values from a channel then send it to
+    another.*)
+  | Send of expression * expression
+  | Receive of expression * expression
+  (*Send and receive between two channels*)
+  | Exchange of expression * expression
+
+  | Str of string
+*)

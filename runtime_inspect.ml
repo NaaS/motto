4dafc3faeb3e7294c794e15f9e3dd1ccdeeb9598
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-07-11 01:20:32 +0100

    started elaborating file loading;

--- runtime_inspect.ml
+++ runtime_inspect.ml
@@ -106,12 +106,16 @@ let eval (st : state) (ctxt : Runtime_data.runtime_ctxt) (i : inspect_instructio
     let ctxt'' =
       { ctxt' with Runtime_data.value_table = (v, value) :: ctxt'.Runtime_data.value_table } in
     (st, ctxt'')
+  | Load file_path ->
+    let st', _ =
+      Compiler.parse_program file_path
+      |> Compiler.front_end ~st:st Config.cfg in
+    (st', ctxt(*FIXME*))
 (*FIXME currently unsupported
   | Declare_channel v ->
   | Close_channel v ->
   | Q_channel (v, e_s) ->
   | Deq_channel v ->
-  | Load file_path ->
   | Eval e_s ->
   | MI mi ->
 *)

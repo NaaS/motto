f3f6a05b4a8c5e14621566f1e7a82ad1da2e45dd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-07-11 03:03:18 +0100

    generalised, to be usable for the scriptable runtime;

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+DEFAULT_TARGET=otto.byte
+
+if [ -z $1 ]
+then
+  TARGET=${DEFAULT_TARGET}
+else
+  TARGET="${1}.byte"
+fi
+
+echo "building ${TARGET}"
+
 # NOTE could add -dont-catch-errors to have exceptions pass through catches.
 ocamlbuild -cflag -g -lflag -g -tag thread -use-ocamlfind -use-menhir \
   -package core \
   -no-hygiene \
-  otto.byte
+  ${TARGET}

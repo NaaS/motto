7efc301a960e3390bd61c9c10cf384a65f4973a4
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-10-02 16:12:47 +0100

    added file which should have been added with 64a17b8cdec543d4a123f494889b0787b4baa58e;

---
+++ front-end/functions.ml
@@ -0,0 +1,65 @@
+(*
+   Defined functions in Flick.
+   Nik Sultana, Cambridge University Computer Lab, September 2015
+*)
+(*FIXME ensure that interpreted fnctors never get declared by the user*)
+
+
+open Crisp_syntax
+
+exception Functions_Exc of string(*message*) * string(*function name*) * expression option
+
+type function_entry =
+  {
+    (*Function name*)
+    name : function_name;
+    (*Function type*)
+    ty : function_type;
+    (*Function implementation (in OCaml)*)
+    impl : expression list -> expression;
+  }
+
+type function_db = function_entry list
+
+let function_db : function_db =
+[{name = "head";
+  ty = FunType (FunDomType ([], [List (Some "l", Undefined, None, [])]), FunRetType [Undefined]);
+  impl = (fun es ->
+    match es with
+    | [ConsList (h, _)] -> h
+    | [e] -> failwith "Function not defined for given input"(*FIXME give more details*)
+    | _ -> failwith "Wrong number of arguments"(*FIXME use Functions_Exc*)
+    )
+};
+ {name = "tail";
+  ty = FunType (FunDomType ([], [List (Some "l", Undefined, None, [])]), FunRetType [List (None, Undefined, None, [])]);
+  impl = (fun es ->
+    match es with
+    | [ConsList (_, t)] -> t
+    | [e] -> failwith "Function not defined for given input"(*FIXME give more details*)
+    | _ -> failwith "Wrong number of arguments"(*FIXME use Functions_Exc*)
+    )
+}]
+
+let export_fun (e : function_entry) : (function_name * (bool * function_type)) =
+  (e.name, (true, e.ty))
+
+let export_funs : (function_name * (bool * function_type)) list =
+  List.map export_fun function_db
+
+(*
+let lookup_function (fn : function_name) : function_entry option =
+  let result =
+    List.filter (fun e -> e.name = fn) function_db in
+  match result with
+  | [] -> None
+  | [e] -> Some e
+  | _ -> raise (Functions_Exc ("Found multiple definitions", fn, None))
+*)
+let apply_function (fn : function_name) (args : expression list) : expression =
+  let result =
+    List.filter (fun e -> e.name = fn) function_db in
+  match result with
+  | [] -> raise (Functions_Exc ("Could not find definitions", fn, None))
+  | [e] -> e.impl args
+  | _ -> raise (Functions_Exc ("Found multiple definitions", fn, None))

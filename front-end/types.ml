677e6f28363784f9e58fb52294ee05fcd8384786
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-10-19 11:06:37 +0100

    added missing module that deals with interpreted, but not native, types -- just as the Functions module deals with functions;

---
+++ front-end/types.ml
@@ -0,0 +1,37 @@
+(*
+   Defined types.
+   Nik Sultana, Cambridge University Computer Lab, October 2015
+*)
+(*FIXME ensure that interpreted symbols never get declared by the user*)
+(*FIXME might want to include backend-specific meaning for these in relevant
+        backend modules only.*)
+
+
+open Crisp_syntax
+open Naasty
+
+exception Types_Exc of string(*message*) * string(*function name*) * expression option
+
+type type_entry =
+  {
+    (*Type name*)
+    name : label;
+    (*Flick type*)
+    ty_opt : type_value option;
+    (*IL type*)
+    naasty_ty_opt : naasty_type option;
+  }
+
+let task_event_ty = Literal_Type (None, "TaskEvent")
+
+let types =
+  [
+    let naasty_ty = Literal_Type (None, "TaskEvent") in
+    { name = "TaskEvent";
+      ty_opt = Some (IL_Type naasty_ty);
+      naasty_ty_opt = Some naasty_ty;
+    };
+  ]
+
+let export_types =
+  List.map (fun {name; (*ty_opt;*) naasty_ty_opt} -> (name, naasty_ty_opt)) types

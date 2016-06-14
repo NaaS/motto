692bbdab7c7be49ea6176434a9d2366185ca5414
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-06-15 00:15:22 +0100

    colourified output, and added instruction numbers for the scriptable runtime;

--- general/debug.ml
+++ general/debug.ml
@@ -21,3 +21,33 @@ let stringify (x : 'a) : string =
   else if Obj.tag (Obj.repr x) = Obj.int_tag then
     string_of_int (Obj.magic x)
   else failwith "stringify over unknown type"
+
+type colour =
+ | Black
+ | Red
+ | Green
+ | Yellow
+ | Blue
+ | Magenta
+ | Cyan
+ | White
+
+let int_of_colour = function
+ | Black -> 0
+ | Red -> 1
+ | Green -> 2
+ | Yellow -> 3
+ | Blue -> 4
+ | Magenta -> 5
+ | Cyan -> 6
+ | White -> 7
+
+let ansi_prefix = "\027[3"
+let encolour (c : colour) (s : string) : string =
+  ansi_prefix ^ string_of_int (int_of_colour c) ^ "m" ^ s
+
+let foreground_colour (c : colour) : unit =
+  ansi_prefix ^ string_of_int (int_of_colour c) ^ "m"
+  |> print_string
+
+let reset_colour = "\027[0;22m"
